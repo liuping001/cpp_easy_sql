a929343852e32c7b8f2721a3cc396a17b137af2a
root <root@DESKTOP-ULBRDBF.localdomain>
2019-07-16 20:46:27 +0800

    get result

--- cpp_xml.py
+++ cpp_xml.py
@@ -4,129 +4,153 @@
 from xml.dom import *
 import xml.dom.minidom
 import re
+import sys
+import os
 
-type_convert = {"int32":"int32_t",  "array[int32]":"std::vector<int32_t>",
-                "uint32":"uint32_t", "array[uint32]":"std::vector<uint32_t>",
-                "string": "std::string", "array[string]":"std::vector<std::string>",
-                "uint64":"uint64_t",  "array[uint64]":"std::vector<uint64_t>",
-                "int64":"int64_t",  "array[int64]":"std::vector<int64_t>",
+type_convert = {"int32":"int32_t",          "array[int32]":"std::vector<int32_t>",
+                "uint32":"uint32_t",        "array[uint32]":"std::vector<uint32_t>",
+                "string": "std::string",    "array[string]":"std::vector<std::string>",
+                "uint64":"uint64_t",        "array[uint64]":"std::vector<uint64_t>",
+                "int64":"int64_t",          "array[int64]":"std::vector<int64_t>",
+                "double":"double",          "array[double]":"std::vector<double>",
+                "bool":"bool",
                 }
 
-def makeStruct(tag):
+type_convert_sql_heander = {"int32":"Int", "uint32":"UInt",
+                            "int64":"Int64", "uint64":"UInt64",
+                            "string":"String", "double":"Double",
+                            "bool":"Boolean"}
+
+def make_struct(tag):
     str = "struct " + tag.nodeName + " {\n"
     for item in tag.getElementsByTagName("field"):
         str += "\t" + type_convert[item.getAttribute("type")] + " " + item.getAttribute("name") + ";\n"
-    str += "};\n"
+    str += "};\n\n"
     return str
 
-def printSS(str, head = "\t"):
+def print_ss(str, head = "\t"):
     return head + "ss << \" " + str + "\";\n"
 
-def printOneValue(str):
+def print_one_value(str):
     return "\'\" + to_string(" + str + ") + \"\'"
 
-def printValue(str):
+def print_value(str):
     return "\'\" + to_string(" + str + ") + \"\'"
 
 class PreFix:
-    def prefixStr(self, matched):
+    def prefix_str(self, matched):
         value = matched.group('value')
-        return printValue(self.prefix + value[2:len(value)-1])
+        return print_value(self.prefix + value[2:len(value)-1])
 
-    def replaceField(self, data, prefix):
+    def replace_field(self, data, prefix):
         self.prefix = prefix
-        return (re.sub('(?P<value>#{[0-9a-zA-Z_]{1,}})', self.prefixStr, data))
-def RePrefix(data, prefix):
-    return PreFix().replaceField(data, prefix)
+        return (re.sub('(?P<value>#{[0-9a-zA-Z_]{1,}})', self.prefix_str, data))
+def re_prefix(data, prefix):
+    return PreFix().replace_field(data, prefix)
 
 class PreFixCppField:
-    def prefixStr(self, matched):
+    def prefix_str(self, matched):
         value = matched.group('value')
         return self.prefix + value[2:len(value)-1]
 
-    def replaceField(self, data, prefix):
+    def replace_field(self, data, prefix):
         self.prefix = prefix
-        return (re.sub('(?P<value>#{[0-9a-zA-Z_]{1,}})', self.prefixStr, data))
-def RePreFixCppField(data, prefix):
-    return PreFixCppField().replaceField(data, prefix)
+        return (re.sub('(?P<value>#{[0-9a-zA-Z_]{1,}})', self.prefix_str, data))
+def re_prefix_cpp_field(data, prefix):
+    return PreFixCppField().replace_field(data, prefix)
 
-def dealTextToSql(data, field_prefix, head = ""):
+def deal_text_to_sql(data, field_prefix, head = ""):
     str = ""
     t = data.split("\n") # 每一行处理成一行
     for item in t:
         item = item.replace('\n', ' ')
         item = re.sub(' +', ' ', item)
-        item = RePrefix(item, field_prefix)
+        item = re_prefix(item, field_prefix)
         if (item != "" and item.isspace() == False):
-            str += printSS(item, head)
+            str += print_ss(item, head)
     return str
 
-def parseCondation(tag, level = 1):
+# 解析具体标签 if、foreach、text
+def parse_condation(tag, level = 1):
     head=""
     for count in range(0, level):
         head += "\t"
     str = ""
     if tag.nodeName == "if":
         str += head + "if ("
-        str += RePreFixCppField(tag.getAttribute("test"), "param.")
+        str += re_prefix_cpp_field(tag.getAttribute("test"), "param.")
         str += ") {\n"
         for if_tag in tag.childNodes:
-            str += parseCondation(if_tag, level + 1)
+            str += parse_condation(if_tag, level + 1)
         str += head + "}\n"
     elif tag.nodeName == "foreach":
         collection = tag.getAttribute("collection")
-        collection = RePreFixCppField(collection, "param.")
+        collection = re_prefix_cpp_field(collection, "param.")
         open = tag.getAttribute("open")
         close = tag.getAttribute("close")
         separator = tag.getAttribute("separator")
 
-        str += printSS(open, head)
+        str += print_ss(open, head)
         str += head + "for (size_t i = 0; i < " + collection + ".size(); i++) {\n"
-        str += head + "\tif (i != 0) {" + printSS(separator, "\n\t\t" + head) + head + "\t}\n"
-        str += printSS(printOneValue(collection + "[i]"), head + "\t")
+        str += head + "\tif (i != 0) {" + print_ss(separator, "\n\t\t" + head) + head + "\t}\n"
+        str += print_ss(print_one_value(collection + "[i]"), head + "\t")
         str += head + "}\n"
-        str += printSS(close, head)
+        str += print_ss(close, head)
 
     elif tag.nodeType == 3:
         if (tag.data != "" and tag.data.isspace() == False):
-            str += dealTextToSql(tag.data, "param.", head)
+            str += deal_text_to_sql(tag.data, "param.", head)
     return str
 
-need_deal_action = ("select", "update", "insert", "delete")
+def cpp_header_include(include):
+    return "#include<" + include + ">\n"
+def cpp_using_namespace(namespace):
+    return "using namespace " + namespace + ";\n"
 
-dom = xml.dom.minidom.parse(r'./test.xml')
-root = dom.documentElement
+class ParseXml:
+    def __init__(self, xml_file):
+        self.need_deal_action = ("select", "update", "insert", "delete")
+        self.param_class_sql = ""
+        self.result_class_sql = ""
+        self.make_sql_func = ""
+        self.make_result_func = ""
+        self.include_header_string = ""
+        self.dom = xml.dom.minidom.parse(xml_file)
+        self.root = self.dom.documentElement
+        self.get_namespace()
+        self.cpp_header()
+        self.parse_xml()
+    def draw(self, out_file):
+        with open(out_file, 'w+') as f:
+            f.write(self.include_header_string)
+            f.write("\nnamespace " + self.namespace +" {\n\n")
+            f.write(self.param_class_sql)
+            f.write("\n")
+            f.write(self.result_class_sql)
+            f.write("\n")
+            f.write(self.make_sql_func)
+            f.write("\n")
+            f.write(self.make_result_func)
+            f.write("}\n")
 
-# string to_string
-print "#include<string>\n"
-print "#include<sstream>\n"
-print "#include<vector>\n"
-print "using namespace std;\n\n"
+    def get_namespace(self):
+        tag = self.root.getAttribute("namespace")
+        self.namespace = tag
 
-print "std::string to_string(const std::string &s) {  return s; }\n"
+    def cpp_header(self):
+        self.include_header_string = "#pragma once\n"
+        self.include_header_string += cpp_header_include("string")
+        self.include_header_string += cpp_header_include("sstream")
+        self.include_header_string += cpp_header_include("vector")
+        self.include_header_string += cpp_using_namespace("std")
+        # string to_string
+        self.include_header_string += "inline std::string to_string(const std::string &s) {  return s; }\n"
 
-for name in need_deal_action:
-    action = root.getElementsByTagName(name)
-    for item in action:
-        # 生成方法的请求参数
-        param_name = item.getAttribute("param_type")
-        param_type = root.getElementsByTagName(param_name)
-        if (len(param_type) == 1):
-            print makeStruct(param_type[0])
-        elif (len(param_type) == 0):
-            param_name = ""
+    def get_sql_func_name(self, name):
+        return name + "Sql"
 
-        # 生成方法的返回结果
-        result_name = item.getAttribute("result_type")
-        result_type = root.getElementsByTagName(result_name)
-        if (len(result_type) == 1):
-            print makeStruct(result_type[0])
-        elif (len(result_type) == 0):
-            result_name = ""
-
-        # 生成产生sql的函数
-        sql_func_name = item.getAttribute("id")
-        sql_func_content = "std::string " + sql_func_name + "Sql"
+    def make_get_sql_func(self, item, param_name):
+        sql_func_content = "std::string " + self.get_sql_func_name(item.getAttribute("id"))
         if (param_name):
             sql_func_content += " (const " + param_name + " &param)"
         else:
@@ -136,8 +160,87 @@ for name in need_deal_action:
         sql_func_content += "\t"
         sql_func_content += "ostringstream ss;\n"
         for item_sql in item.childNodes:
-            sql_func_content += parseCondation(item_sql)
+            sql_func_content += parse_condation(item_sql)
         sql_func_content += "\treturn ss.str();\n"
-        sql_func_content += "}\n"
-        print sql_func_content
+        sql_func_content += "}\n\n"
+        self.make_sql_func += sql_func_content
 
+    def make_get_result_func_by_name(self, item, result_name, param_name):
+        self.make_result_func += "\n"
+        # 返回值
+        return_type = ""
+        if result_name == "":
+            return_type = "int32_t"
+        else:
+            return_type = "std::vector<%s>"%(result_name)
+        self.make_result_func += return_type
+
+        # 函数名
+        sql_func_name = item.getAttribute("id")
+        sql_func_name += "ResultByName"
+        self.make_result_func += " " + sql_func_name
+
+        # 参数值
+        if param_name == "":
+            self.make_result_func += " (SqlHander *sql_header)"
+        else:
+            self.make_result_func += " (SqlHander *sql_header, const " + param_name + " &param)"
+        self.make_result_func += " {\n"
+
+        head = "\t"
+        #定义返回值
+        if result_name != "":
+            self.make_result_func += head + return_type + " ret;\n"
+        #获取sql
+        self.make_result_func += head + "auto sql = " + self.get_sql_func_name(item.getAttribute("id")) + "(param);\n"
+        #执行sql
+        self.make_result_func += head + "sql_header->ExecuteSql(sql);\n"
+
+        #不是sql这里就已经返回了
+        if result_name == "":
+            self.make_result_func += head + "return sql_header->GetFetchSize();\n}\n"
+            return
+
+        self.make_result_func += head + "while(sql_header->Next()) {\n"
+        self.make_result_func += head + "\t ret.emplace_back();\n"
+        tag = self.root.getElementsByTagName(result_name)[0]
+        for item in tag.getElementsByTagName("field"):
+            self.make_result_func += "\t\tret.back().%s = sql_header->Get%s(\"%s\");\n"%(item.getAttribute("name"),
+                                     type_convert_sql_heander[item.getAttribute("type")], item.getAttribute("name"))
+        self.make_result_func += head +"}\n"
+        self.make_result_func += head + "return ret;\n}\n"
+
+    def parse_xml(self):
+        for name in self.need_deal_action:
+            action = self.root.getElementsByTagName(name)
+            for item in action:
+                # 生成方法的请求参数
+                param_name = item.getAttribute("param_type")
+                param_type = self.root.getElementsByTagName(param_name)
+                if (len(param_type) == 1):
+                    self.param_class_sql += make_struct(param_type[0])
+                elif (len(param_type) == 0):
+                    param_name = ""
+
+                # 生成方法的返回结果
+                result_name = item.getAttribute("result_type")
+                result_type = self.root.getElementsByTagName(result_name)
+                if (len(result_type) == 1):
+                    self.result_class_sql += make_struct(result_type[0])
+                elif (len(result_type) == 0):
+                    result_name = ""
+
+                # 生成产生sql的函数
+                self.make_get_sql_func(item, param_name)
+
+                # 生成执行函数（输入结构体，返回结果结构体）
+                self.make_get_result_func_by_name(item, result_name, param_name)
+def main():
+    for arg in sys.argv[1:]:
+        print "process begin : " + arg
+        parse = ParseXml(arg)
+        parse.draw(os.path.splitext(arg)[0] + "_sql.h")
+        print "finish : " + arg
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ sql_hander.h
@@ -0,0 +1,31 @@
+#include <string>
+
+// 需要实现的接口
+struct SqlHandler {
+    virtual void ExecuteSql(const std::string &sql) = 0;
+    virtual bool Next() = 0;
+
+    virtual size_t GetFetchSize() = 0;
+    virtual size_t GetRow() = 0;
+
+    virtual bool GetBoolean(uint32_t column_index) const = 0;
+    virtual bool GetBoolean(const sql::string& column_label) const = 0;
+
+    virtual long double GetDouble(uint32_t column_index) const = 0;
+    virtual long double GetDouble(const sql::string& column_label) const = 0;
+
+    virtual int32_t GetInt(uint32_t column_index) const = 0;
+    virtual int32_t GetInt(const std::string& column_label) const = 0;
+
+    virtual uint32_t GetUInt(uint32_t column_index) const = 0;
+    virtual uint32_t GetUInt(const std::string& column_label) const = 0;
+
+    virtual int64_t GetInt64(uint32_t column_index) const = 0;
+    virtual int64_t GetInt64(const std::string& column_label) const = 0;
+
+    virtual uint64_t GetUInt64(uint32_t column_index) const = 0;
+    virtual uint64_t GetUInt64(const std::string& column_label) const = 0;
+
+    virtual std::string GetString(uint32_t column_index)  const = 0;
+    virtual std::string GetString(const std::string& column_label) const = 0;
+};
